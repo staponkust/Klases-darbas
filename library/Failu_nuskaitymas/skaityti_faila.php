2877e995bf8110e580042162db06e58222ab0f14
staponkust <staponkust@gmail.com>
2021-12-29 22:58:03 +0200

    Nuskaitomas failas ir pakeiciamas tekstas nurodytu tekstu

---
+++ library/Failu_nuskaitymas/skaityti_faila.php
@@ -0,0 +1,15 @@
+<?php
+
+/*$kiek = filesize('duomenys.html');
+$resursas = fopen('duomenys.html', 'r');
+$resursas1 = fopen('nauji_duomenys.html', 'x+');
+
+$failotekstas = fread($resursas,$kiek);
+
+$pakeistas_tekstas = preg_replace("/Pasauli/", '<span style="background:yellow">Tomai</span>', $failotekstas);
+fwrite($resursas1,$pakeistas_tekstas);
+fclose($resursas);
+fclose($resursas1);*/
+
+
+?>
